2fff60ecabde01b81788344dd5d817719208454c
Alex Buchkin <alexbuchkin@protonmail.com>
2020-12-31 00:06:02 +0400

    messages storing added

--- __main__.py
+++ __main__.py
@@ -4,6 +4,11 @@ import requests
 import sys
 import time
 
+import logging
+import psycopg2
+
+logging.basicConfig(level=logging.INFO)
+
 MAIN_URL = 'https://api.telegram.org/bot{token}/'
 GET_UPDATES_URL = MAIN_URL + 'getUpdates'
 SEND_MESSAGE_URL = MAIN_URL + 'sendMessage'
@@ -13,23 +18,60 @@ class RepeaterBot:
     def __init__(
         self,
         token,
-        last_update_ts=None,
+        database_url,
+        database_settings,
     ):
-        self.last_update_ts = last_update_ts or 0
         self.update_request_url = GET_UPDATES_URL.format(token=token)
         self.send_message_url = SEND_MESSAGE_URL.format(token=token)
+
+        if database_url:
+            self.conn = psycopg2.connect(database_url)
+        else:
+            if any((not value for value in database_settings.values())):
+                raise KeyError('Some fields from database settings are missing')
+            self.conn = psycopg2.connect(**database_settings)
+
+        logging.info('Connected to database')
+
+        with self.conn.cursor() as cursor:
+            cursor.execute(
+                '''
+                CREATE TABLE IF NOT EXISTS messages
+                (
+                    message VARCHAR(1000),
+                    chat_id BIGINT NOT NULL,
+                    ts BIGINT NOT NULL
+                );
+                '''
+            )
+            self.conn.commit()
+            logging.info('Table has been created')
+
+        with self.conn.cursor() as cursor:
+            cursor.execute(
+                '''
+                SELECT MAX(ts)
+                FROM messages;
+                '''
+            )
+            result = cursor.fetchone()
+            self.last_update_ts = result[0] if result else 0
+        logging.info('last_update_ts is set to {}'.format(str(self.last_update_ts)))
 
     def send_update_request(
         self,
     ):
+        logging.info('Sending request to: {}'.format(self.update_request_url))
         response = requests.get(
             url=self.update_request_url,
         )
         if not response or not response.ok:
+            logging.info('not response or not response.ok')
             return {}
         try:
             return response.json()
         except ValueError:
+            logging.info('responsing json is not valid')
             return {}
 
     def get_new_messages(
@@ -38,11 +80,14 @@ class RepeaterBot:
     ):
         if not response_json.get('ok') or \
            not response_json.get('result'):
+            logging.info('"result" or "ok" is not valid')
             return []
+
         message_list = (item['message'] for item in response_json['result'] if item.get('message'))
+
         return [
             {
-                'text': message.get('text', 'This message has no text'),
+                'text': message.get('text'),
                 'chat_id': message['chat']['id'],
                 'ts': message['date'],
             }
@@ -64,6 +109,8 @@ class RepeaterBot:
         chat_id,
         text,
     ):
+        text = text or 'This message has no text'
+        logging.info('Sending message "{}" to chat_id "{}"'.format(text, str(chat_id)))
         requests.get(
             url=self.send_message_url,
             params={
@@ -72,18 +119,52 @@ class RepeaterBot:
             },
         )
 
+    def store_message(
+        self,
+        chat_id,
+        text,
+        ts,
+    ):
+        with self.conn.cursor() as cursor:
+            cursor.execute(
+                '''
+                INSERT INTO messages
+                VALUES (%(message)s, %(chat_id)s, %(ts)s);
+                ''',
+                {
+                    'message': text[:1000] if text else None,
+                    'chat_id': chat_id,
+                    'ts': ts,
+                }
+            )
+            self.conn.commit()
+
     def loop(self):
         while True:
             new_messages = self.get_new_messages(self.send_update_request())
+            logging.info('{} new messages'.format(str(len(new_messages))))
+
             for message in new_messages:
                 self.send_message(
                     chat_id=message['chat_id'],
                     text=message['text'],
                 )
 
+                self.store_message(
+                    chat_id=message['chat_id'],
+                    text=message['text'],
+                    ts=message['ts'],
+                )
+
             self.update_last_ts(new_messages)
 
             time.sleep(1)
+
+    def __exit__(self):
+        self.cursor.close()
+        self.conn.close()
+
+        logging.info('Disconnected from database')
 
 
 @click.command()
@@ -94,12 +175,62 @@ class RepeaterBot:
     type=str,
 )
 @click.option(
-    '--last-update-ts',
-    envvar='LAST_UPDATE_TS',
-    type=int,
+    '--database-url',
+    envvar='DATABASE_URL',
+    type=str,
 )
-def main(bot_token, last_update_ts):
-    bot = RepeaterBot(token=bot_token, last_update_ts=last_update_ts)
+@click.option(
+    '--database-name',
+    envvar='DATABASE_NAME',
+    type=str,
+)
+@click.option(
+    '--database-user',
+    envvar='DATABASE_USER',
+    type=str,
+)
+@click.option(
+    '--database-password',
+    envvar='DATABASE_PASSWORD',
+    type=str,
+)
+@click.option(
+    '--database-host',
+    envvar='DATABASE_HOST',
+    type=str,
+    default='localhost',
+)
+@click.option(
+    '--database-port',
+    envvar='DATABASE_PORT',
+    type=str,
+    default='5432',
+)
+def main(
+    bot_token,
+    database_url,
+    database_name,
+    database_user,
+    database_password,
+    database_host,
+    database_port,
+):
+    database_settings = {
+        'dbname': database_name,
+        'user': database_user,
+        'password': database_password,
+        'host': database_host,
+        'port': database_port,
+    }
+
+    logging.info('Creating bot')
+    bot = RepeaterBot(
+        token=bot_token,
+        database_url=database_url,
+        database_settings=database_settings,
+    )
+    logging.info('Bot has been created')
+
     bot.loop()
 
 
